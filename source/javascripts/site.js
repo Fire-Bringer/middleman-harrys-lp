a9b0f64251eb94757fd7280bedb366929f10a1d8
Rashad DuPaty <radupaty@gmail.com>
2024-05-10 19:38:09 +0900

    fixed javascript to events section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,25 @@
-// This is where it all goes :)
+document.addEventListener('DOMContentLoaded', function() {
+
+  console.log("Hello from the Info Controller");
+
+  let scrollContainer = document.querySelector(".gallery");
+  let backBtn = document.getElementById("backBtn");
+  let nextBtn = document.getElementById("nextBtn");
+
+  scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehavior = "auto";
+  });
+
+  nextBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 900;
+  });
+
+  backBtn.addEventListener("click", ()=>{
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 900;
+  });
+
+});
